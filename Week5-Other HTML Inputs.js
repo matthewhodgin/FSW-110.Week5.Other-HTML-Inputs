73b328594e99faea74a0d53dd838a385b4097071
matthewhodgin <95104960+matthewhodgin@users.noreply.github.com>
2022-03-05 11:09:46 -0700

    Add files via upload

--- Week5-Other HTML Inputs.js
+++ Week5-Other HTML Inputs.js
@@ -1,15 +1,34 @@
 const form = document.myForm
 
+document.getElementById('text1').style.marginLeft= "5px";
+document.getElementById('text2').style.marginLeft= "5px";
+document.getElementById('experience').style.marginBottom= "20px";
+
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-    
-    console.log(form.decision.value)
+
+    alert(`
+    First Name: ${form.text1.value} 
+    Last Name: ${form.text2.value}
+    Age: ${form.text3.value}
+    Welcomed: ${form.decision.value}
+    How was your experience?:  ${form.experience.value}
+    How did you hear about us?:  ${hearAbout()}`);
 })
 
+const checkedInputs = [];
+    function hearAbout(){
+    for(let i = 0; i < form.choice.length; i++){
+        if(form.choice[i].checked){
+            checkedInputs.push(form.choice[i].value);
+        }
+    }
+    return(checkedInputs)
+}
 
-//if (alert("Fist name: Joe", "Last name: Shmoe","Age: 31","Gender: Male")) 
-//    {
-//  } 
 
 
-alert("First Name: Joe\nLast Name: Shmoe\nAge: 31\nGender: Male\nLocation: Sahara Desert\nDietary Resrictions: Kosher, Vegetarian");
+
+
+
+ 
